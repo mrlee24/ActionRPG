10a26f31ab2edb9840d14560866eb69bff718503
mrlee24 <30282894+mrlee24@users.noreply.github.com>
2022-03-25 12:00:36 -0400

    RPGPawn
    Added RPGPawn

---
+++ Source/ActionRPG/Private/Pawns/RPGPawn.cpp
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Pawns/RPGPawn.h"
+
+#include "Animations/Components/RPGLocomotionComponent.h"
+#include "Pawns/RPGPawnMovementComponent.h"
+
+
+// Sets default values
+ARPGPawn::ARPGPawn()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	RPGPawnMovementComponent = CreateDefaultSubobject<URPGPawnMovementComponent>(TEXT("Pawn Movement Component"));
+	LocomotionComponent = CreateDefaultSubobject<URPGLocomotionComponent>(TEXT("Locomotion Component"));
+}
+
+// Called when the game starts or when spawned
+void ARPGPawn::BeginPlay()
+{
+	Super::BeginPlay();
+
+	LocomotionComponent->GetLocomotionStateNativeChanged().AddUObject(this, &ThisClass::HandleLocomotionStateChanged);		
+}
+
+// Called every frame
+void ARPGPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ARPGPawn::HandleLocomotionStateChanged(const FRPGLocomotionState& newLocomotionState)
+{
+	RPGPawnMovementComponent->CurrentLocomotionState = newLocomotionState;
+}
+
---
+++ Source/ActionRPG/Public/Pawns/RPGPawn.h
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Interfaces/RPGMovableInterface.h"
+#include "RPGPawn.generated.h"
+
+UCLASS()
+class ACTIONRPG_API ARPGPawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ARPGPawn();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	virtual void HandleLocomotionStateChanged(const struct FRPGLocomotionState& newLocomotionState);
+
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = Components)
+	class URPGLocomotionComponent* LocomotionComponent = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = Components)
+	class URPGPawnMovementComponent* RPGPawnMovementComponent = nullptr;
+};
